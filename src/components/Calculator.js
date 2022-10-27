1e66cc9ef8bf17119dc6b99e097146ffd32cccf5
vitalis <vitalismutwiri2gmail.com>
2022-10-27 11:07:03 +0300

    Add hooks to stateles functional component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,64 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import { buttonsa, operators } from './buttons';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: 0,
-      next: null,
-      operation: null,
-    };
-    this.handleClick = this.handleClick.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-  }
-
-  handleClick = (e) => {
+const Calculator = () => {
+  const [obj, setState] = useState({ total: 0, next: null, operation: null });
+  const handleClick = (e) => {
     const value = e.target.name;
-    const result = calculate(this.state, value);
-    this.setState(result);
-  }
+    const result = calculate(obj, value);
+    setState(result);
+  };
 
-  handleUpdate = (e) => {
-    this.setState({ total: e.target.value });
-  }
-
-  render() {
-    const { total } = this.state;
-    const { next } = this.state;
-    const { operation } = this.state;
-    return (
-      <div className="container">
-        <div className="wrapper">
-          <div className="wrapper_top">
-            <p className="output" onChange={this.handleUpdate}>
-              { total}
-              {operation}
-              { next }
-            </p>
-          </div>
-          <div className="wrapper_middle">
-            <div className="buttonsa">
-              {
+  const handleUpdate = (e) => {
+    setState({ total: e.target.value });
+  };
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <div className="wrapper_top">
+          <p className="output" onChange={handleUpdate}>
+            { obj.total}
+            {obj.operation}
+            { obj.next }
+          </p>
+        </div>
+        <div className="wrapper_middle">
+          <div className="buttonsa">
+            {
               buttonsa.map((data) => (
-                <button type="button" key={data.name} name={data.value} onClick={this.handleClick} id={data.name}>{data.value}</button>
+                <button type="button" key={data.name} name={data.value} onClick={handleClick} id={data.name}>{data.value}</button>
               ))
             }
-            </div>
-            <div className="operations">
-              {
+          </div>
+          <div className="operations">
+            {
               operators.map((data) => (
-                <button type="button" key={data.name} name={data.value} onClick={this.handleClick} id={data.name}>{data.value}</button>
+                <button type="button" key={data.name} name={data.value} onClick={handleClick} id={data.name}>{data.value}</button>
               ))
             }
-            </div>
           </div>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Calculator;
